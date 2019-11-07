de3a5df104bb55762286e7cbcb015908cf222ddb
Administrator <345443631@qq.cm>
2019-11-07 11:12:25 +0800

    20191101

---
+++ app/src/main/java/com/darleer/elgin/test/testrxjava/activity/CityActivity.java
@@ -0,0 +1,14 @@
+package com.darleer.elgin.test.testrxjava.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import com.darleer.elgin.test.testrxjava.R;
+
+public class CityActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_city);
+    }
+}
