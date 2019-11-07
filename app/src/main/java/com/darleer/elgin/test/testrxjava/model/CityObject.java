860a41d4598105d5262fba26d60174b089d768c3
Administrator <345443631@qq.cm>
2019-11-07 11:10:06 +0800

    20191101

---
+++ app/src/main/java/com/darleer/elgin/test/testrxjava/model/CityObject.java
@@ -0,0 +1,10 @@
+package com.darleer.elgin.test.testrxjava.model;
+
+import java.util.List;
+
+public class CityObject {
+    public String cityName;
+    public List<StationObject> cityStations;
+}
+
+
---
+++ app/src/main/java/com/darleer/elgin/test/testrxjava/model/StationObject.java
@@ -0,0 +1,7 @@
+package com.darleer.elgin.test.testrxjava.model;
+
+public class StationObject
+{
+    public String station_name;
+    public String station_code;
+}
